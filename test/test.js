804de3610d5c60005edbf17ae20f70c5678e49bd
TheMalaland <rodrigochiozalde@gmail.com>
2025-01-17 15:55:33 -0600

    new branch created using now The internet

--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
+//It is possible this proyect meets captcha issues. If you have any problem, please let me know.
+//This is a simple test that uses the Selenium WebDriver to perform a Google search and validate the results.
+//The test suite includes the following tests:
+//1. Perform a basic search and validate the results.
+//2. Test that the URL matches the expected URL.
+//3. Test that the title matches the expected title.
+//4. Test that special characters can be entered in the input field.
+
 import { Builder, By, until } from 'selenium-webdriver'; // Import necessary modules from selenium-webdriver
 import { expect } from 'chai'; // Import the expect assertion library from chai
 import 'chromedriver'; // Import chromedriver to manage Chrome browser
 
-describe('Google Search Tests', function () { // Describe the test suite
+describe('Website ', function () { // Describe the test suite
   this.timeout(30000);
   let driver; // Declare the driver variable
 
@@ -19,7 +27,7 @@ describe('Google Search Tests', function () { // Describe the test suite
   });
 
   it('should perform a basic search and validate results', async function () {
-    await driver.get('https://www.google.com');
+    await driver.get('https://the-internet.herokuapp.com/');
     const searchBox = await driver.findElement(By.name('q'));
     await searchBox.sendKeys('Mundial México 2026');
     await searchBox.submit();
@@ -36,5 +44,14 @@ describe('Google Search Tests', function () { // Describe the test suite
     const title = await driver.getTitle();
     expect(title).to.equal('Google');
   });
+
+  it('should test special characters in the input field', async function () {
+    await driver.get('https://www.google.com');
+    const searchBox = await driver.findElement(By.name('q'));
+    await searchBox.sendKeys('!@#$%^&*()');
+    await searchBox.submit();
+    await driver.wait(until.titleContains('!@#$%^&*()'), 10000);
+  });
+
 
 });
