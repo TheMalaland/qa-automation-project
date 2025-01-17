7bbc1b42e614b1b1be6529cc86913a229a96d047
TheMalaland <rodrigochiozalde@gmail.com>
2025-01-17 14:43:47 -0600

    added two tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,20 @@
-import { Builder, By, until } from 'selenium-webdriver';
-import { expect } from 'chai';
+import { Builder, By, until } from 'selenium-webdriver'; // Import necessary modules from selenium-webdriver
+import { expect } from 'chai'; // Import the expect assertion library from chai
+import 'chromedriver'; // Import chromedriver to manage Chrome browser
 
-describe('Google Search Tests', function () {
-  let driver;
+describe('Google Search Tests', function () { // Describe the test suite
+  this.timeout(30000);
+  let driver; // Declare the driver variable
 
-  before(async function () {
+
+  // Before all tests, initialize the WebDriver
+  before(async function () { 
+    // Build the WebDriver for Chrome browser
     driver = await new Builder().forBrowser('chrome').build();
   });
 
-  after(async function () {
+   // After all tests, quit the WebDriver
+  after(async function () { 
     await driver.quit();
   });
 
@@ -17,6 +23,18 @@ describe('Google Search Tests', function () {
     const searchBox = await driver.findElement(By.name('q'));
     await searchBox.sendKeys('Mundial México 2026');
     await searchBox.submit();
+  });
+
+  it('should test URL matches', async function () {
+    await driver.get('https://www.google.com');
+    const currentUrl = await driver.getCurrentUrl();
+    expect(currentUrl).to.equal('https://www.google.com/');
+  });
+
+  it('should test title matches', async function () {
+    await driver.get('https://www.google.com');
+    const title = await driver.getTitle();
+    expect(title).to.equal('Google');
   });
 
 });
